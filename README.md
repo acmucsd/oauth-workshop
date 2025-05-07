2d41ec408c8067b497781e00626d3226e6d3e2e7
Angela Hu <angelahu925@gmail.com>
2025-05-07 16:21:59 -0700

    Moved starter

---
+++ README.md
@@ -0,0 +1,7 @@
+# oauth-workshop
+Starter code for ACM Hack's Authentication Workshop
+# Google Oauth
+Created by Kris Foster [(Video)](https://www.youtube.com/watch?v=Q0a0594tOrc)
+
+# Node.js User Auth Login
+Created by Web Dev Simplified [(Video)](https://www.youtube.com/watch?v=Ud5xKCYQTjM)
---
+++ login/server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const bcrypt = require('bcrypt')
+
+app.use(express.json())
+
+const users = []
+
+app.get('/users', (req, res) => {
+  res.json(users)
+})
+
+app.post('/users', async (req, res) => {
+  try {
+    // TODO Activity 1
+    res.status(201).send()
+  } catch {
+    res.status(500).send()
+  }
+})
+
+app.post('/users/login', async (req, res) => {
+  const user = users.find(user => user.name === req.body.name)
+  if (user == null) {
+    return res.status(400).send('Cannot find user')
+  }
+  try {
+    // TODO Activity 1
+  } catch {
+    res.status(500).send()
+  }
+})
+
+app.listen(3000)
