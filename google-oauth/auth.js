60f200ed48bce08c44ef112af643f16d19f147b6
Sarah Li <100035287+sli051357@users.noreply.github.com>
2025-05-07 17:07:22 -0700

    updated dotenv dependencies

--- google-oauth/auth.js
+++ google-oauth/auth.js
@@ -1,5 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
+const dotenv = require('dotenv');
+dotenv.config();
 
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
